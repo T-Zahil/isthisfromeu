cb834a068bb84d4a3d936bad828c6354aa4ff3d1
Thomas Sanlis <Thomas.sanlis@gmail.com>
2025-03-25 14:39:29 +0100

    Update website title

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,8 @@
 <script setup>
 useSeoMeta({
-  title: 'Is it European? - Check brand origins',
+  title: 'Is this from EU? - Check brand origins',
   description: 'Find out if a brand is European or not. A simple application to check product origins.',
-  ogTitle: 'Is it European? - The app to verify brand origins',
+  ogTitle: 'Is this from EU? - The app to verify brand origins',
   ogDescription: 'Instantly find out if a brand is European or not with our search tool.',
   ogImage: '/og-image.png',
   twitterCard: 'summary_large_image'
